1576cf35793734e7d34c764b08b7dbd6af87a908
FaqodAqomaddin <faqod.aqomaddin.ft21@mail.umy.ac.id>
2022-11-18 17:22:14 +0700

    add loop node inserted at the end of the list

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -64,6 +64,16 @@ namespace double_linked_list
              * between which the new node is to be inserted*/
             newNode.next = current;
             newNode.prev = previous;
+
+            //if the node is to be inserted at the end of the list
+            if (current == null)
+            {
+                newNode.next = null;
+                previous.next = newNode;
+                return;
+            }
+            current.prev = newNode;
+            previous.next = newNode;
         }
     }
 }
