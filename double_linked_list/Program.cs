bfe99340b6d4b5aa6b22c7c2eec03f6d009ebdf5
FaqodAqomaddin <faqod.aqomaddin.ft21@mail.umy.ac.id>
2022-11-18 16:50:39 +0700

    Add class Node

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System;
+
+namespace double_linked_list
+{
+    class Node
+    {
+        /*Node class represent the node of doubly linked list
+         * it consists of the information part and and links to
+         * its succeding and preceeding
+         * in term of next and previous */
+        public int noMhs;
+        publik string name;
+        //point to the succeding node
+        public Node next;
+        public Node prev;
+    }
+}
